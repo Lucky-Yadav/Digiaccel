39e35e7bc06c513b8515b1df1e3699d2622f486f
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-02 16:52:26 +0530

    update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 
 function App() {
+  const drawerWidth = 240;
+  const navItems = ["Home", "user", "Admin"];
+
+
+  
   return (
     <div className="App">
-      
+      <Box sx={{ display: 'flex' }}>
+      <AppBar component="nav">
+        <Toolbar>
+          
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </div>
   );
 }
